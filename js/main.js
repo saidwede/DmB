8d046e499df6de6c23130057190af6e97638e8be
SAID BIO WEDE <saidbiowede@gmail.com>
2023-09-14 19:09:10 +0100

    Home page V1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(document).on("click", ".qw-card", function(){
-    $(".qw-card.is-active").removeClass("is-active");
-    $(this).addClass("is-active");
+    $(".qw-card.is-active .answer").slideUp(500);
+    setTimeout(() => {
+        $(".qw-card.is-active").removeClass("is-active");
+        $(this).find(".answer").slideDown(500);
+        $(this).addClass("is-active");
+    }, 500);
+})
+
+$(document).on("click", ".burger-menu-btn *", function(){
+    $(".mobile-top-menu").css("height", "260px");
+    $(".mobile-top-menu-overlay").css("display", "block");
+})
+$(document).on("click", ".mobile-top-menu-overlay", function(){
+    $(".mobile-top-menu").css("height", "0px");
+    $(".mobile-top-menu-overlay").css("display", "none");
 })
