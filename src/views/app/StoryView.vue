a76aacdb99f939a5a0c3bf5cdb5522e1e2e9f712
SAID BIO WEDE <saidbiowede@gmail.com>
2024-08-06 12:08:54 +0100

    History added

--- src/views/app/StoryView.vue
+++ src/views/app/StoryView.vue
@@ -16,7 +16,12 @@ const uiStore = useUiStore()
         <i class="fa-solid fa-chevron-left" style="margin-right: 10px"></i><span>Retour</span>
       </button>
     </RouterLink>
-    <h1>Histoire II</h1>
-    <div></div>
+    <div class="story-container">
+      <h1>Le Roi Behanzin</h1>
+      <p>Il était une fois, dans un pays lointain d'Afrique, un roi très courageux appelé Behanzin. Il était le roi du Dahomey, un pays plein de soleil, de forêts et de guerriers.  Behanzin était un grand chef, il aimait beaucoup son peuple et il se battait pour les protéger des envahisseurs.</p>
+      <p>Un jour, des hommes venus d'un pays très lointain sont arrivés dans le Dahomey. Ils voulaient prendre le contrôle du pays et de ses richesses. Behanzin, brave comme un lion, a refusé de se laisser faire ! Il a mené ses guerriers au combat et a lutté avec courage pour défendre son royaume.</p>
+      <p>Même si les envahisseurs étaient nombreux, les guerriers du Dahomey se sont battus avec fierté. Ils étaient très forts et très habiles au combat, et ils portaient de belles tenues et des casques colorés. Mais malheureusement, les envahisseurs étaient plus puissants et Behanzin a été vaincu.</p>
+      <p>Behanzin a été emprisonné loin de son pays. Mais même en prison, il restait un grand chef. Il n'a jamais abandonné l'espoir de revoir son peuple et de le rendre libre.  L'histoire de Behanzin nous rappelle qu'il faut toujours se battre pour ce qui est juste, même quand les choses sont difficiles. </p>
+    </div>
   </div>
 </template>
