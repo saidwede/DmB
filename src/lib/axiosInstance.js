ae67885f3ba8eaa095312b24217437c79f2e30f5
SAID BIO WEDE <saidbiowede@gmail.com>
2024-08-06 17:55:56 +0100

    Timeout set to 10s

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL, // replace with your API base URL
-  timeout: 1000, // optional
+  timeout: 10000, // optional
   headers: {
     'Content-Type': 'application/json',
     // Add any other custom headers if needed
